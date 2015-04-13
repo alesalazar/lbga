69a6e22bf576e10ca1b6de3c906a474e67f07480
Ale Salazar <alejandra.salazar@socialweb.cl>
2015-04-13 16:59:46 -0300

    se agregan archivos de lbga

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*-----------------BANNER INICIO--------------------*/
+
+/*-----------------FIN DE BANNER INICIO--------------------*/
+
+
+/*-----------------CADENA ADN INVESTIGACION-----------------*/
+$('#myTab a:first').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTab a:second').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTab a:third').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTab a:fourth').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('#myTab a:fifth').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+/*---------------FIN CADENA ADN INVESTIGACION----------------*/
+
+
+
+
+
+
+
+
+
